1a25a8d31df13dfa78847d0eabb28949dacd3d08
ImranNajirSohan <sohanuddin404@gmail.com>
2024-02-17 23:35:05 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Validator;
+
+Route::get('/', function (){
+    return view('pages.home');
+});
+
+Route::resource('user',UserController::class);
+
